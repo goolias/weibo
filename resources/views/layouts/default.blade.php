1b401877edf641204c6df7a2c4637d096a30665a
vagrant <vagrant@homestead>
2019-07-29 10:58:03 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,8 +10,12 @@
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
+
+  
+    </div>
       </div>
     </div>
   </body>
